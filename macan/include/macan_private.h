f09199a0bc0c07fdb7b330f965cfaa2a9c4440a6
Michal Sojka <sojkam1@fel.cvut.cz>
2013-11-26 20:32:50 +0100

    Attempt to add more debuging - seems we have a problem in bitfield ordering

--- macan/include/macan_private.h
+++ macan/include/macan_private.h
@@ -30,8 +30,13 @@
 
 /* MaCAN message definitions */
 
+#define FL_CHALLENGE 1
+#define FL_SESS_KEY  2
+#define FL_ACK       2
+#define FL_SIGNAL    3
+
 struct macan_crypt_frame {
-	uint8_t flags : 2;
+	uint8_t flags : 2;	/* FIXME: Bitfield ordering is not (probably) defined and is does not match to the MaCAN paper */
 	uint8_t dst_id : 6;
 };
 
