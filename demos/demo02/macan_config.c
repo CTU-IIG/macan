268431a8117af751e74c8b6d2117962b1a162f0b
Michal Sojka <sojkam1@fel.cvut.cz>
2013-11-27 15:58:50 +0100

    Convert demo02 to the new configuration

--- demos/demo02/macan_config.c
+++ demos/demo02/macan_config.c
@@ -21,7 +21,6 @@
  *  along with MaCAN.	If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <macan.h>
 #include "macan_config.h"
 
 /* ToDo: more recipients */
@@ -32,4 +31,38 @@ struct macan_sig_spec demo_sig_spec[] = {
 	[SIGNAL_B]    = {.can_nsid = 0,   .can_sid = 202, .src_id = NODE1,       .dst_id = NODE2, .presc = 0}, // signal 2
 	[SIGNAL_C]    = {.can_nsid = 103, .can_sid = 0,   .src_id = NODE1,       .dst_id = NODE2, .presc = 2}, // signal 3
 	[SIGNAL_D]    = {.can_nsid = 104, .can_sid = 204, .src_id = NODE1,       .dst_id = NODE2, .presc = 5}, // signal 4
+};
+
+const uint32_t ecu2canid_map[] = {
+	[KEY_SERVER] = 0,
+	[TIME_SERVER] = 1,
+	[NODE1] = 2,
+	[NODE2] = 3,
+};
+
+
+#define SIG_TIME 4
+#define TIME_DELTA 2000   /* tolerated time divergency from TS in usecs */
+#define TIME_DIV 500000
+#define TIME_TIMEOUT 5000000	/* usec */
+#define SKEY_TIMEOUT 6000000000u /* usec */
+#define SKEY_CHG_TIMEOUT 30000000u /* usec */
+#define ACK_TIMEOUT 10000000	  /* usec */
+
+struct macan_config config = {
+	.node_id = 2,
+	.ltk = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F },
+	.sig_count = SIG_COUNT,
+	.sigspec = demo_sig_spec,
+	.node_count = NODE_COUNT,
+	.ecu2canid = ecu2canid_map,
+	.key_server_id = KEY_SERVER,
+	.time_server_id = TIME_SERVER,
+	.can_id_time = SIG_TIME,
+	.time_div = TIME_DIV,
+	.ack_timeout = ACK_TIMEOUT,
+	.skey_validity = SKEY_TIMEOUT,
+	.skey_chg_timeout = SKEY_CHG_TIMEOUT,
+	.time_timeout = ACK_TIMEOUT,
+	.time_delta = TIME_DELTA,
 };
