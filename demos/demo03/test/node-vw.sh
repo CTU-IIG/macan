911d657243e8eae2e240afa46d365c5c8c926caa
Michal Sojka <sojkam1@fel.cvut.cz>
2013-12-13 15:46:29 +0100

    node-vw: Pass stdin to macangw after sending initialization commands

--- demos/demo03/test/node-vw.sh
+++ demos/demo03/test/node-vw.sh
@@ -12,5 +12,5 @@ echo "BASH: Starting VW node"
     echo "time"
     sleep 1
     echo "req_auth:2:1"
-    sleep 100000
+    cat
 ) | $PATH_TO_VW_BIN/macangw -o 3 -t 500 -b can0 -e vw_can -k can0 -c $PATH_TO_VW_CONFIG/ecuvw.json -s $PATH_TO_VW_CONFIG/signals_ecuvw.json
