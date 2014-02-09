51dfcbdba49f21523f81c221771585de6d62c05b
Ondrej Kulaty <kulaty.o@gmail.com>
2014-02-09 22:45:41 +0100

    Add support for cryptlib to tricore specific code

--- macan/src/target/tricore/tc_macan.c
+++ macan/src/target/tricore/tc_macan.c
@@ -70,68 +70,6 @@ int write(int s, struct can_frame *cf, int len)
 	return 16;
 }
 
-/**
- * checks a message authenticity
- *
- * The function computes CMAC of the given plain text and compares
- * it against cmac4. Returns 1 if CMACs matches.
- *
- * @param skey  128-bit session key
- * @param cmac4: points to CMAC message part, i.e. 4 bytes CMAC
- * @param plain: plain text to be CMACked and checked against
- * @param len:   length of plain text in bytes
- */
-int check_cmac(struct macan_ctx *ctx, uint8_t *skey, const uint8_t *cmac4, uint8_t *plain, uint8_t *fill_time, uint8_t len)
-{
-	/* TODO: This is not target specific - move to the common code base */
-	uint8_t cmac[16];
-	uint64_t time;
-	uint32_t *ftime = (uint32_t *)fill_time;
-	int i;
-
-	if (!fill_time) {
-		aes_cmac(skey, len, cmac, plain);
-
-		return memchk(cmac4, cmac, 4);
-	}
-
-	time = macan_get_time(ctx);
-
-	for (i = -1; i <= 1; i++) {
-		*ftime = time + i;
-		aes_cmac(skey, len, cmac, plain);
-
-		if (memchk(cmac4, cmac, 4) == 1)
-			return 1;
-	}
-
-	return 0;
-}
-
-/**
- * sign() - signs a message with CMAC
- * @skey:  128-bit session key
- * @cmac4: 4 bytes of the CMAC signature will be written to
- * @plain: a plain text to sign
- * @len:   length of the plain text
- */
-void sign(uint8_t *skey, uint8_t *cmac4, uint8_t *plain, uint8_t len)
-{
-	uint8_t cmac[16];
-	aes_cmac(skey, len, cmac, plain);
-
-	memcpy(cmac4, cmac, 4);
-}
-
-/**
- * unwrap_key() - deciphers AES-WRAPed key
- */
-void unwrap_key(const uint8_t *key, size_t len, uint8_t *dst, uint8_t *src)
-{
-	aes_set_key(key);
-	aes_unwrap(key, len, dst, src, src);
-}
-
 uint64_t read_time()
 {
 	uint64_t time;
