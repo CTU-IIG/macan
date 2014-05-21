ac0c9805c4a1cc7b0c9a473e9a9d54e176f1b31e
Michal Sojka <sojkam1@fel.cvut.cz>
2014-05-21 10:39:47 +0200

    Initialize vcan interfaces in the "attack" test

--- test/attack/run.sh
+++ test/attack/run.sh
@@ -3,6 +3,15 @@
 cd $(dirname $0)
 PATH=$PWD/../../_compiled/bin-tests:$PWD/../../_compiled/bin:$PATH
 
+vcan() {(
+	set -x
+	sudo ip link add $1 type vcan
+	sudo ip link set $1 up
+)}
+
+[ -e /sys/class/net/vcani ] || vcan vcani
+[ -e /sys/class/net/vcanj ] || vcan vcanj
+
 trap 'kill $attacker' EXIT
 
 attacker-gw & attacker=$!
