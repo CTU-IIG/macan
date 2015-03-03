4d66e7872ad5c816d748ae30c48106f58a80cb7a
Michal Sojka <sojkam1@fel.cvut.cz>
2015-03-03 23:42:28 +0100

    Replace ks and ts with ksts in the cosummit demo

---
+++ demos/cosummit/test/ksts.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source ./path.sh
+export MACAN_DUMP=1 # Serve as macanmon too
+$PATH_TO_CTU_BIN/macan_ksts -c $PATH_TO_CTU_CONFIG/libdemo_cosummit_cfg.so -k $PATH_TO_CTU_CONFIG/libdemo_cosummit_keys.so
