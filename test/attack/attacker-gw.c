d197f1ff30f564a016cda5e2be915496ee1e4dc7
Michal Sojka <sojkam1@fel.cvut.cz>
2015-05-13 20:08:07 +0200

    Fix printing in attacker test

--- test/attack/attacker-gw.c
+++ test/attack/attacker-gw.c
@@ -4,12 +4,14 @@
 #include <linux/can.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <macan_private.h>
+#include <macan.h>
 
 int sock_i, sock_j;
+struct macan_ctx *ctx;
 
 /* macan_config is only needed here for decoding the frames for printing. */
 extern struct macan_config config;
-void print_frame(const struct macan_config *cfg, struct can_frame *cf, const char *prefix);
 
 struct can_frame remember_ch_i;
 
@@ -21,7 +23,7 @@ static void can_rx_cb (struct ev_loop *loop, ev_io *w, int revents)
 	struct can_frame cf;
 
 	while (read(w->fd, &cf, sizeof(cf)) == sizeof(cf)) {
-		print_frame(&config, &cf, " ");
+		print_frame(ctx, &cf, " ");
 
 		if (w->fd == sock_i) {
 			if (attack && cf.can_id == 0x102 && cf.data[0] == 0x40 && cf.data[1] == 0x03) {
@@ -30,7 +32,7 @@ static void can_rx_cb (struct ev_loop *loop, ev_io *w, int revents)
 
 				/* (5.1.3) */
 				struct can_frame m = { .can_id = 0x103, .can_dlc = 8, .data = {0x40, 0x02, 0, 0, 0, 0, 0, 0} };
-				print_frame(&config, &m, "!!! REPLACED WITH");
+				print_frame(ctx, &m, "!!! REPLACED WITH");
 				write(sock_j, &m, sizeof(m));
 				continue;
 			}
@@ -40,7 +42,7 @@ static void can_rx_cb (struct ev_loop *loop, ev_io *w, int revents)
 				struct can_frame m = cf;
 				m.can_id = 0x103;
 				//m.data[0] = 0x82;
-				print_frame(&config, &m, "!!! REPLAY AS j");
+				print_frame(ctx, &m, "!!! REPLAY AS j");
 				write(sock_j, &m, sizeof(m));
 				continue;
 			}
@@ -50,7 +52,7 @@ static void can_rx_cb (struct ev_loop *loop, ev_io *w, int revents)
 				/* (5.1.6) */
 				printf("!!! REMOVED\n");
 				/* (5.1.7) */
-				print_frame(&config, &remember_ch_i, "!!! REPLAYED");
+				print_frame(ctx, &remember_ch_i, "!!! REPLAYED");
 				write(sock_j, &remember_ch_i, sizeof(cf));
 				continue;
 			}
@@ -64,6 +66,12 @@ int main(int argc, char *argv[])
 {
 	(void)argc; (void)argv;
 	struct ev_loop *loop = EV_DEFAULT;
+	struct macan_node_config nc = {
+		.node_id = 0xff, /* Invalid ID */
+	};
+
+
+	ctx = macan_alloc_mem(&config, &nc);
 
 	ev_io can_i, can_j;
 	ev_io_init (&can_i, can_rx_cb, sock_i = helper_init("vcani"), EV_READ);
